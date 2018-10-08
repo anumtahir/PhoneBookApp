cf9cbe775bb7b6f7b763a2e67fbdd4eba4034e8f
Anum Tahir <anumtahir242@gmail.com>
2018-10-08 05:52:03 -0700

    two classes of Person.cs and Contact.cs

---
+++ WebApplication4/WebApplication4/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string ContactNumber { get; set; }
+        public string Type { get; set; }
+        public int PersonId { get; set; }
+       
+}
+}
